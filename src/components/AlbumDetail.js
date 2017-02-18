f3934837d79a3a54e8046358b7cb8ce07c8cda24
Dan Kreiger <dan@dankreiger.com>
2017-02-18 14:05:56 +0100

    Destructure jsx

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import { Text, Image } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 
-const AlbumDetail = (props) => (
-  <Card>
-    <Text>{props.album.title}</Text>
-    <Image
-      source={{ uri: props.album.image }}
-      style={{ width: 40, height: 40 }}
-    />
-  </Card>
-);
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+  const { headerContentStyle, thumbnailStyle } = styles;
+
+  return (
+    <Card>
+      <CardSection>
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
+  );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+};
 
 
 export default AlbumDetail;
